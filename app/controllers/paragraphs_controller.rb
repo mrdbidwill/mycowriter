d14b6a87188d9013b49dff4a10e43b2ddd75a8df
Will Johnston <mrdbidwill@gmail.com>
2026-02-15 19:05:36 -0600

    Section and Paragraph controllers implemented! Both controllers now have: create - Add new sections/paragraphs update - Edit existing content destroy - Delete sections/paragraphs move_up / move_down - Reorder using acts_as_list The editor buttons are now fully wired up and ready to use.

--- app/controllers/paragraphs_controller.rb
+++ app/controllers/paragraphs_controller.rb
@@ -1,2 +1,59 @@
 class ParagraphsController < ApplicationController
+  before_action :set_section
+  before_action :set_paragraph, only: [:update, :destroy, :move_up, :move_down]
+
+  def create
+    @paragraph = @section.paragraphs.build(paragraph_params)
+    @paragraph.position = @section.paragraphs.maximum(:position).to_i + 1
+
+    if @paragraph.save
+      render json: {
+        id: @paragraph.id,
+        content: @paragraph.content,
+        position: @paragraph.position
+      }, status: :created
+    else
+      render json: { errors: @paragraph.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @paragraph.update(paragraph_params)
+      render json: {
+        id: @paragraph.id,
+        content: @paragraph.content
+      }
+    else
+      render json: { errors: @paragraph.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @paragraph.destroy
+    head :no_content
+  end
+
+  def move_up
+    @paragraph.move_higher
+    render json: { position: @paragraph.position }
+  end
+
+  def move_down
+    @paragraph.move_lower
+    render json: { position: @paragraph.position }
+  end
+
+  private
+
+  def set_section
+    @section = Section.find(params[:section_id])
+  end
+
+  def set_paragraph
+    @paragraph = @section.paragraphs.find(params[:id])
+  end
+
+  def paragraph_params
+    params.require(:paragraph).permit(:content)
+  end
 end
--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -1,2 +1,59 @@
 class SectionsController < ApplicationController
+  before_action :set_book
+  before_action :set_section, only: [:update, :destroy, :move_up, :move_down]
+
+  def create
+    @section = @book.sections.build(section_params)
+    @section.position = @book.sections.maximum(:position).to_i + 1
+
+    if @section.save
+      render json: {
+        id: @section.id,
+        title: @section.title,
+        position: @section.position
+      }, status: :created
+    else
+      render json: { errors: @section.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @section.update(section_params)
+      render json: {
+        id: @section.id,
+        title: @section.title
+      }
+    else
+      render json: { errors: @section.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @section.destroy
+    head :no_content
+  end
+
+  def move_up
+    @section.move_higher
+    render json: { position: @section.position }
+  end
+
+  def move_down
+    @section.move_lower
+    render json: { position: @section.position }
+  end
+
+  private
+
+  def set_book
+    @book = Book.find(params[:book_id])
+  end
+
+  def set_section
+    @section = @book.sections.find(params[:id])
+  end
+
+  def section_params
+    params.require(:section).permit(:title)
+  end
 end
